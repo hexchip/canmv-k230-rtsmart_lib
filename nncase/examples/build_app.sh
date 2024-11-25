e3b0a3a0b07ead2fcb5ae1eb8f55d26b46b23423
kendryte747 <kendryte747@gmail.com>
2024-11-25 17:44:32 +0800

    Fix build error.

--- nncase/examples/build_app.sh
+++ nncase/examples/build_app.sh
@@ -2,7 +2,7 @@
 set -x
 
 # set cross build toolchain
-export PATH=$PATH:/opt/toolchain/riscv64-linux-musleabi_for_x86_64-pc-linux-gnu/bin/
+export PATH="$PATH:~/.kendryte/k230_toolchains/riscv64-linux-musleabi_for_x86_64-pc-linux-gnu/bin"
 
 clear
 rm -rf out
