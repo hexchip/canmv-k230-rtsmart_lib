ce57cc0e8374cceefeeaac560b0ae77cf2c5a6ba
xiaenling <xiaenling@canaan-creative.com>
2025-04-18 13:46:20 +0800

    Update libnncase

--- mk/libnncase.mk
+++ mk/libnncase.mk
@@ -2,7 +2,8 @@
 nncase_inc_dir :=
 nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/libs/nncase/riscv64
 nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/libs/nncase/riscv64/nncase/include
+nncase_inc_dir += $(SDK_RTSMART_SRC_DIR)/libs/nncase/riscv64/rvvlib/include
 
 NNCASE_INC = $(addprefix -I, $(nncase_inc_dir))
-NNCASE_LIBS = -lNncase.Runtime.Native -lnncase.rt_modules.k230 -lfunctional_k230 -lstdc++
-NNCASE_LIB_DIR = -L$(SDK_RTSMART_SRC_DIR)/libs/nncase/riscv64/nncase/lib
+NNCASE_LIBS = -lNncase.Runtime.Native -lnncase.rt_modules.k230 -lfunctional_k230 -lrvv -lstdc++
+NNCASE_LIB_DIR = -L$(SDK_RTSMART_SRC_DIR)/libs/nncase/riscv64/nncase/lib -L$(SDK_RTSMART_SRC_DIR)/libs/nncase/riscv64/rvvlib
