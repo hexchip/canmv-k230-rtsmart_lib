ac49cc6762a4164bad40b23d68376ab87e8abf61
wangyan1998 <1011344314@qq.com>
2025-07-31 17:05:15 +0800

    update opencv.mk

--- mk/libopencv.mk
+++ mk/libopencv.mk
@@ -5,4 +5,4 @@ lib_dir += $(SDK_RTSMART_SRC_DIR)/libs/opencv/lib/opencv4/3rdparty
 
 LIB_CFLAGS += $(addprefix -I, $(inc_dir))
 LIB_LDFLAGS += $(addprefix -L, $(lib_dir)) 
-LIB_LDFLAGS += -Wl,--start-group -lstdc++ -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lzlib -llibjpeg-turbo -llibopenjp2 -llibpng -llibtiff -llibwebp -lcsi_cv -latomic -Wl,--end-group
+LIB_LDFLAGS += -Wl,--start-group -lstdc++ -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_videoio -lopencv_calib3d -lopencv_features2d -lopencv_flann -lzlib -llibjpeg-turbo -llibopenjp2 -llibpng -llibtiff -llibwebp -lcsi_cv -latomic -Wl,--end-group
